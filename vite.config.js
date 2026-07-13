f6486d5f22be149e3092038bedce3977633f3478
Amine Mami <130773489+TheCongres@users.noreply.github.com>
2026-07-13 15:39:56 +0100

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // The base path MUST match your repository name
-  base: "/e-plantShopping/", 
+  base: "/", 
   plugins: [react()],
 })
